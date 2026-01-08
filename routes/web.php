04a4d18240fb075738c741f70053600713922a57
Carlos <zamarra107@gmail.com>
2026-01-08 23:01:26 +0100

    Creada lista de juegos general

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GameController;
 
-Route::get('/', function () {
-    return view('listaGeneral');
-});
+Route::get('/', [GameController::class, 'index']);
+
+// Ruta placeholder para la ficha (tu compañero puede reemplazarla por su controlador/vista)
+Route::get('/juego/{id}', function ($id) {
+    return "Ficha de juego #{$id} (pendiente de implementar)";
+})->name('juego.show');
